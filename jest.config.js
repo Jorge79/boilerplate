a3530a3f924571441fd55c60810b06419ffd060d
Jorge Francisco <Jorge7.9@hotmail.com>
2022-05-19 11:03:45 -0300

    chore: add jest absolute path for modules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
